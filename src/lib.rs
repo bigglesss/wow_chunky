e1186be35608cc45831fdcc05d5d3597a9c10038
biggle <>
2022-10-07 19:01:38 +0100

    cleanup lib description

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! A pure Rust parsing library for World of Warcraft's chunked files, intended to support all TLV-chunked files from versions 1.12 to 3.3.5
+//! A pure Rust parsing library for World of Warcraft's chunked files (and some additional binary files), targeting versions 1.12 to 3.3.5
 //! 
 //! Supported formats
 //! ------------------
@@ -13,20 +13,22 @@
 //!
 //! Examples
 //! -----------
+//!
 //! 
 //! ```rust
-//!     // Load and parse the ADT at (25, 20) in the Azeroth map.
-//!     let wdt_path = PathBuf::from("./test_data/Azeroth/Azeroth.wdt");
-//!     let adt = wow_chunky::files::ADT::from_wdt_file(wdt_path, 25, 20);
+//! // Load and parse the ADT at (25, 20) in the Azeroth map.
+//! let wdt_path = PathBuf::from("./test_data/Azeroth/Azeroth.wdt");
+//! let adt = wow_chunky::files::ADT::from_wdt_file(wdt_path, 25, 20);
 //! ```
 //! 
 //! ```rust
-//!     // Load and parse the WDT first.
-//!     let wdt_path = PathBuf::from("./test_data/Azeroth/Azeroth.wdt");
-//!     let wdt = wow_chunky::parser::wdt::WDT::from_file(wdt_path);
+//! // Load and parse the WDT first.
+//! let wdt_path = PathBuf::from("./test_data/Azeroth/Azeroth.wdt");
+//! let wdt = wow_chunky::parser::wdt::WDT::from_file(wdt_path);
 //! 
-//!     // Then, using the flags we need from the WDT (required for heightmap parsing), parse the ADT at (31, 30).
-//!     let adt = wow_chunky::files::ADT::from_wdt(wdt, 31, 30)
+//! // Then, using the flags we need from the WDT (required for heightmap parsing),
+//! // parse the ADT at (31, 30).
+//! let adt = wow_chunky::files::ADT::from_wdt(wdt, 31, 30)
 //! ```
 
 pub mod chunks;
