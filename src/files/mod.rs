4bc934a1d6c45dd67503d78b9b77a42144800c5b
biggle <>
2022-10-07 20:26:09 +0100

    remove unused parse_bls method

--- src/files/mod.rs
+++ src/files/mod.rs
@@ -1,6 +1,4 @@
 //! Parsing logic and base structs for all chunked file formats.
-use std::path::PathBuf;
-
 use binread::{BinReaderExt, io::Cursor};
 
 use crate::error::Error;
@@ -28,13 +26,4 @@ fn parse_chunk_data_args<T: binread::BinRead>(chunk_data: &Vec<u8>, args: T::Arg
     let chunk_data: T = chunk_data_cursor.read_le_args(args).map_err(Error::Unknown)?;
 
     Ok(chunk_data)
-}
-
-pub fn parse_bls(path: PathBuf) -> Result<bls::BLS, Error> {
-    let file = std::fs::read(path).map_err(Error::IO)?;
-
-    let mut cursor = Cursor::new(file);
-
-    let parsed_bls: bls::BLS = cursor.read_le()?;
-    return Ok(parsed_bls);
 }
