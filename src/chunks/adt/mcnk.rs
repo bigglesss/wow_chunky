86eac7eece0d9ec11fcd3f818c1656a3ed83d295
biggle <>
2022-10-07 19:05:47 +0100

    link to Namigator code in heightmap parser

--- src/chunks/adt/mcnk.rs
+++ src/chunks/adt/mcnk.rs
@@ -230,11 +230,12 @@ static ADT_SIZE: f32 = 533.0 + (1.0 / 3.0);
 static QUAD_SIZE: f32 = ADT_SIZE / 128.0;
 
 pub fn parse_heightmap(raw: Vec<f32>, offset:shared::C3Vector) -> Vec<shared::C3Vector> {
-    // TODO: This currently uses the distant view heightmap, which causes some irregularities.
-    // I also suspect the scaling is not the same as the blizz client, perhaps it need to be scaled up.
     let mut parsed: Vec<shared::C3Vector> = Vec::new();
     for (i, height) in raw.iter().enumerate() {
-        // if i % 17 > 8, this is the inner part of a quad
+        // From https://github.com/namreeb/namigator/blob/master/parser/Adt/Chunks/MCNK.cpp#L117.
+        // TODO: This currently uses the distant view heightmap, which causes some irregularities.
+
+        // If i % 17 > 8, this is the inner part of a quad.
         let inner = (i % 17) > 8;
 
         let i: i32 = i.try_into().unwrap();
